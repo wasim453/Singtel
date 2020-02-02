da19a71f8a6473bd2d3f9220779e801da47ea692
hp <hp@DESKTOP-3S2KJMM>
2020-02-02 20:18:40 +0800

    B1 Answer

---
+++ assignment/src/main/java/org/signtel/com/assignment/model/Fish.java
@@ -0,0 +1,20 @@
+package org.signtel.com.assignment.model;
+/**
+ * 
+ * @author Wasim Akbar
+ *
+ */
+public class Fish{
+	
+	public void sing() {
+		System.out.println("I dont sing");
+	}
+	
+	public void walk() {
+		System.out.println("I dont walk");
+	}
+	
+	public void swim() {
+		System.out.println("I can swim");
+	}
+}
---
+++ assignment/src/main/java/org/signtel/com/assignment/solution/QuestionB1.java
@@ -0,0 +1,13 @@
+package org.signtel.com.assignment.solution;
+
+import org.signtel.com.assignment.model.Fish;
+
+public class QuestionB1 {
+
+	public static void main(String [] ags) {
+		Fish fish = new Fish();
+		fish.walk();
+		fish.sing();
+		fish.swim();
+	}
+}
