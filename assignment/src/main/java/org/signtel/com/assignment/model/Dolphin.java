dfe077cb830efdfcafc7003893928cdb0e9f6aee
hp <hp@DESKTOP-3S2KJMM>
2020-02-02 20:44:57 +0800

    B3 Answer

---
+++ assignment/src/main/java/org/signtel/com/assignment/model/Dolphin.java
@@ -0,0 +1,15 @@
+package org.signtel.com.assignment.model;
+
+/**
+ * 
+ * @author Wasim Akbar
+ *
+ */
+public class Dolphin {
+
+	Fish fish = new Fish();
+	
+	public void swim() {
+		fish.swim();
+	}
+}
